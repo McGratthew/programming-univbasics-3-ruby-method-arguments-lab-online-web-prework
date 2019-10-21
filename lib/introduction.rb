371a128622341cc9c274542d796a8afce595c3a9
Matt Miller <mbmiller0507@yahoo.com>
2019-10-21 19:51:57 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -5,7 +5,10 @@ end
 introduction("Josh")
 
 def introduction_with_language(name, language)
-  puts ""
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+
+introduction_with_language("Dan","Ember.js")
 
 
 
