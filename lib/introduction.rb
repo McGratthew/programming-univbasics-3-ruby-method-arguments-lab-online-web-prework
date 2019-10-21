193115e7a1db4cb6c00de6f643adedc6f3f15097
Matt Miller <mbmiller0507@yahoo.com>
2019-10-21 19:59:26 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -14,9 +14,7 @@ def introduction_with_language_optional(name, language = 'computer')
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-("Edwin")
-
-introduction_with_language_optional
+introduction_with_language_optional("Edwin")
 
 
 
